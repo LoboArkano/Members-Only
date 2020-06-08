564286fcd30311722e063fbc3831e79b92c66057
Fernando Bahamondes <ferbaco@gmail.com>
2020-06-08 16:32:27 -0400

    Add User model

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :posts
+  validates :name, presence :true, uniqueness: true, length: { maximum: 16, too_long: "%<count>s is the maximum characters allowed"  }
+  validates :status, :email, :encrypted_password,  presence :true
+  validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+
+end
