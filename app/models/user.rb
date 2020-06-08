64f07d0f5222e09e1fa254da80d61f700dc1c6cf
Fernando Bahamondes <ferbaco@gmail.com>
2020-06-08 16:44:39 -0400

    Add Post model

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_many :posts
-  validates :name, presence :true, uniqueness: true, length: { maximum: 16, too_long: "%<count>s is the maximum characters allowed"  }
-  validates :status, :email, :encrypted_password,  presence :true
+  validates :name, presence: true, uniqueness: true, length: { maximum: 16, too_long: "%<count>s is the maximum characters allowed"  }
+  validates :status, :email, :encrypted_password,  presence: true
   validates :email, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 
 end
