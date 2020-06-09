eac1758b357264da24f57d6776f90d2f37e0d8d9
LoboArkano <pipiolo_mananes@hotmail.com>
2020-06-09 10:31:59 -0500

    Add partial view and buttons for sessions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  #before_action :authenticate_user!, only: %i[new create]
+  before_action :authenticate_user!, only: %i[new create]
   include PostsHelper
 
   def new
